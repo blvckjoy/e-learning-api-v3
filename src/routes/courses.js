d8bc5864ae370971b2b59619ca5cc85f0ac1ec59
blvckjoy <sirjeggz@gmail.com>
2025-04-14 23:59:48 +0100

    Add create endpoint

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Course = require("../models/Course");
+const verifyToken = require("../middlewares/auth");
 
 const courseRouter = express.Router();
 
@@ -7,6 +8,32 @@ const courseRouter = express.Router();
 courseRouter.get("/", async (req, res) => {
    const courses = await Course.find();
    res.json(courses);
+});
+
+// Create Course (Only instructor)
+courseRouter.post("/", verifyToken, async (req, res) => {
+   try {
+      if (req.user.role !== "instructor")
+         return res.status(403).json({ message: "Forbidden" });
+
+      const { title, description, duration, price } = req.body;
+      const course = new Course({
+         title,
+         description,
+         duration,
+         price,
+         instructor: req.user.id,
+      });
+      await course.save();
+
+      res.status(201).json({
+         message: "Course created successfully",
+         course: course,
+      });
+   } catch (error) {
+      console.error("Error creating a course:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+   }
 });
 
 module.exports = courseRouter;
