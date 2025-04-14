9e7c4fff648534f475fb3cf6e9a031b8c9f430a2
blvckjoy <sirjeggz@gmail.com>
2025-04-14 21:57:24 +0100

    Add Login endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const authRouter = express.Router();
 
@@ -25,6 +27,32 @@ authRouter.post("/signup", async (req, res) => {
       });
    } catch (error) {
       console.error("Error creating a new user:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+   }
+});
+
+// Login
+authRouter.post("/login", async (req, res) => {
+   try {
+      const { email, password } = req.body;
+      const validUser = await User.findOne({ email });
+      if (!validUser)
+         return res.status(404).json({ message: "User Not Found!" });
+
+      const validPass = await bcrypt.compare(password, validUser.password);
+      if (!validPass)
+         return res.status(400).json({ message: "Invalid credentials" });
+
+      const token = jwt.sign(
+         { id: validUser._id, role: validUser.role },
+         process.env.ACCESS_TOKEN_SECRET,
+         {
+            expiresIn: "2h",
+         }
+      );
+      res.json({ token });
+   } catch (error) {
+      console.error("Error signing in user:", error);
       res.status(500).json({ message: "Internal Server Error" });
    }
 });
