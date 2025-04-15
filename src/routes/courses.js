b7b9668d8b6563eaae2ccd1e34ce60ed59b6f954
blvckjoy <sirjeggz@gmail.com>
2025-04-15 13:24:29 +0100

    feat(courses): implement student removal from courses

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -161,4 +161,44 @@ courseRouter.patch(
    }
 );
 
+// Remove student from a course
+courseRouter.delete(
+   "/:courseId/students/:studentId",
+   verifyToken,
+   async (req, res) => {
+      try {
+         if (req.user.role !== "instructor")
+            return res.status(403).json({ message: "Forbidden" });
+
+         const course = await Course.findById(req.params.courseId);
+         if (!course)
+            return res.status(404).json({ message: "Course Not Found" });
+
+         if (
+            !mongoose.Types.ObjectId.isValid(req.params.courseId) ||
+            !mongoose.Types.ObjectId.isValid(req.params.studentId)
+         )
+            return res.status(400).json({ message: "Invalid ID format" });
+
+         if (!course.students.includes(req.params.studentId))
+            return res
+               .status(400)
+               .json({ message: "Student is not enrolled to this course" });
+
+         course.students = course.students.filter(
+            (studentId) => studentId.toString() !== req.params.studentId
+         );
+         await course.save();
+
+         res.status(200).json({
+            message: "Student removed successfully",
+            course: course,
+         });
+      } catch (error) {
+         console.error("Error removing student:", error);
+         res.status(500).json({ message: "Internal Server Error" });
+      }
+   }
+);
+
 module.exports = courseRouter;
